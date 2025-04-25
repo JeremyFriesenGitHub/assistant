51d9a25454bd4ea22ddd272421207d8d8e2f4719
Matthew M-B <matthew.mb@getjobber.com>
2025-04-25 02:20:24 -0400

    Delete task id print

--- src/ingest.py
+++ src/ingest.py
@@ -1,5 +1,4 @@
 from ingestion.tasks.ingest_webpages_list_task import ingest_webpages_list
 
 if __name__ == "__main__":
-    result = ingest_webpages_list.delay()
-    print(f"Task ID: {result.id}")
+    ingest_webpages_list.delay()
