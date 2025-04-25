0e26e95eafe39a29c0871554e93f023f93459f08
Matthew M-B <matthew.mb@getjobber.com>
2025-04-25 01:00:19 -0400

    Add date columns

--- src/infrastructure/db/models.py
+++ src/infrastructure/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Enum
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey, Enum, func
 from sqlalchemy.orm import declarative_base, relationship
 from pgvector.sqlalchemy import Vector
 
@@ -12,6 +12,8 @@ class Source(Base):
     name = Column(String, nullable=False)
     type = Column(Enum("webpage", "file", name="source_type"), nullable=False)
     reference = Column(String, nullable=False, unique=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
     chunks = relationship("Chunk", back_populates="source")
 
@@ -22,6 +24,8 @@ class Chunk(Base):
     id = Column(Integer, primary_key=True)
     content = Column(String, nullable=False)
     embedding = Column(Vector(384), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
 
     source_id = Column(Integer, ForeignKey("sources.id"), nullable=False)
     source = relationship("Source", back_populates="chunks")
+    
